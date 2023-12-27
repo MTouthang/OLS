9414d30176ebd354d3a857e91e07ff8dace343bc
MTouthang <53576865+MTouthang@users.noreply.github.com>
2023-12-28 01:05:07 +0530

    feat: database connection setup

--- server/app.js
+++ server/app.js
@@ -1,7 +1,29 @@
 import express from 'express';
 import { config } from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
 config();
 
 const app = express();
+
+// built-in Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// third-Party Middleware
+app.use(cors());
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+// server status check route
+app.get('/ping', (_req, res) => {
+  res.send('Pong');
+});
+
+// default catch all route - 404
+app.all('*', (_req, res) => {
+  res.send('OOps!!! 404 Not Found');
+});
 
 export default app;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 import app from './app.js';
+import connectToDB from './configs/dbConnection.js';
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  await connectToDB();
   console.log(`APP is running at http://localhost:${PORT}`);
 });
